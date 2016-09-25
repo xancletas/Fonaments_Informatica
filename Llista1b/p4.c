f8f9c62fe02087c1ca09dc1d20c1846faadd364c
xancletas <jacobfolchsola@gmail.com>
2016-09-25 23:12:12 +0200

    Update p4.c

--- Llista1b/p4.c
+++ Llista1b/p4.c
@@ -27,40 +27,39 @@ int main()
 	}
 
 	//associacio
-	switch (nota < 5)
+	if (nota >= 5)
 	{
-		case (0):
-			if (nota < 10)
+		if (nota < 10)
+		{
+			if (nota < 9)
 			{
-				if (nota < 9)
+				if (nota < 7)
 				{
-					if (nota < 7)
-					{
-						//aprovat
-						printf("%s aprovat.\n",resposta);
-					}
-					else
-					{
-						//notable
-						printf("%s notable.\n",resposta);
-					}
+					//aprovat
+					printf("%s aprovat.\n",resposta);
 				}
 				else
 				{
-					//exelent
-					printf("%s exel.lent.\n",resposta);
-				}	
+					//notable
+					printf("%s notable.\n",resposta);
+				}
 			}
 			else
 			{
-				//matricula
-				printf("%s matricula.\n",resposta);
-			}
-		break;
-		case (1):
-			//suspens
-			printf("%s suspens.\n",resposta);
-		break;
+				//exelent
+				printf("%s exel.lent.\n",resposta);
+			}	
+		}
+		else
+		{
+			//matricula
+			printf("%s matricula.\n",resposta);
+		}
+	}
+	else
+	{
+		//suspens
+		printf("%s suspens.\n",resposta);
 	}
 	return 0;
 }
