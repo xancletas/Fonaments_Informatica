5440128e7dc9cd957ec7b478a702805b7b74e43b
xancletas <jacobfolchsola@gmail.com>
2016-09-28 19:19:16 +0200

    Update p2.c

--- Llista1b/p2.c
+++ Llista1b/p2.c
@@ -22,15 +22,15 @@ int main()
 	if (max-b < 0)
 	{
 		max = b;
-	};
+	}
 	if (max-c < 0)
 	{
 		max = c;
-	};
+	}
 	if (max-d < 0)
 	{
 		max = d;
-	};
+	}
 
 	printf("Dels vlaors [%.2f,%.2f,%.2f,%.2f] el mes gran es: %.2f\n",a,b,c,d,max);
 
